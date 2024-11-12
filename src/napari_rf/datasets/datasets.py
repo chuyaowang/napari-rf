43a8f50c88638dbe6b85b9bb7c4db0399febb97e
psavakis <pesavakis@gmail.com>
2024-11-12 17:12:29 +0100

    added PooledNd2Dataset to factory

--- src/napari_rf/datasets/datasets.py
+++ src/napari_rf/datasets/datasets.py
@@ -2,13 +2,17 @@ import os.path
 
 from napari_rf.datasets.folder_structure_dataset import FolderStructureDataset
 from napari_rf.datasets.nd2_dataset import Nd2Dataset
+from napari_rf.datasets.nd2_pooled_dataset import PooledNd2Dataset
 from napari_rf.datasets.single_image_dataset import SingleImageDataset
 
 
 def get_dataset(cfg):
     if cfg['dataset_path'].lower().endswith('nd2'):
         print('opening nd2 stack')
-        return Nd2Dataset(cfg)
+        if not cfg['return_channels_together']:
+            return Nd2Dataset(cfg)
+        else:
+            return PooledNd2Dataset(cfg)
     elif os.path.isdir(cfg['dataset_path']):
         print('opening folders')
         return FolderStructureDataset(cfg)
