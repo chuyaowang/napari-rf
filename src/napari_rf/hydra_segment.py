d8ae1cae855a9cf2a3bd326bd9d2c98c82a26131
psavakis <pesavakis@gmail.com>
2024-11-12 16:07:18 +0100

    added print statement

--- src/napari_rf/hydra_segment.py
+++ src/napari_rf/hydra_segment.py
@@ -15,7 +15,7 @@ from tqdm import tqdm
 @hydra.main(version_base='1.2', config_path='../../config', config_name='batch_classify_config.yaml')
 def main(cfg: DictConfig) -> Optional[float]:
     working_dir = hydra.core.hydra_config.HydraConfig.get().runtime.output_dir
-
+    print(working_dir)
     dataset = get_dataset(cfg)
     paths = dataset.get_save_structure()
     for p in paths:
