0d3a7e36d84ae3ce90a3d6800b6b211ade83f77e
psavakis <pesavakis@gmail.com>
2024-11-21 12:33:16 +0100

    tidying

--- src/napari_rf/hydra_unpack_stack.py
+++ src/napari_rf/hydra_unpack_stack.py
@@ -8,7 +8,6 @@ warnings.filterwarnings('ignore')
 from napari_rf.datasets.datasets import get_dataset
 from tqdm import tqdm
 
-
 @hydra.main(version_base='1.2', config_path='../../config', config_name='batch_classify_config.yaml')
 def main(cfg: DictConfig) -> Optional[float]:
     working_dir = hydra.core.hydra_config.HydraConfig.get().runtime.output_dir
@@ -23,5 +22,5 @@ def main(cfg: DictConfig) -> Optional[float]:
 
 
 if __name__ == "__main__":
-    sys.argv.append('hydra.run.dir=/media/philipp/seagate5tb/hydra/dataset')
+    # sys.argv.append('hydra.run.dir=/media/philipp/seagate5tb/hydra2/')
     main()
